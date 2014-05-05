fcd4d1556aa44d6837d1500881afc7d6a97b376b
synthresin <synthresin@bagseong-wan-ui-MacBook-Pro.local>
2014-05-06 00:48:21 +0900

    다시

--- lib/generators/su/scaffold_admin_controller_generator.rb
+++ lib/generators/su/scaffold_admin_controller_generator.rb
@@ -19,7 +19,7 @@ module SunshineRailsGenerators
     class_option :orm, banner: "NAME", type: :string, required: true,
                        desc: "ORM to generate the controller for"
 
-    # argument :attributes, type: :array, default: [], banner: "field:type field:type"
+    argument :attributes, type: :array, default: [], banner: "field:type field:type"
 
     def create_controller_files
       template "controller.rb", File.join('app/controllers', controller_class_path, "#{controller_file_name}_controller.rb")
