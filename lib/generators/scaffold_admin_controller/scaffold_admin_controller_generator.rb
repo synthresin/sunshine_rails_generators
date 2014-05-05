735235a8ae624402311f9ff34957cc008216ea57
synthresin <synthresin@bagseong-wan-ui-MacBook-Pro.local>
2014-05-05 19:52:22 +0900

    모듈 바깥으로

--- lib/generators/scaffold_admin_controller/scaffold_admin_controller_generator.rb
+++ lib/generators/scaffold_admin_controller/scaffold_admin_controller_generator.rb
@@ -1,9 +1,9 @@
 #require 'rails/generators/base'
 
-module SunshineRailsGenerators
+#module SunshineRailsGenerators
   class ScaffoldAdminControllerGenerator
     def create_initializer_file
 	    create_file "config/initializers/initializer.rb", "# Add initialization content here"
 	  end
   end
-end
+#end
