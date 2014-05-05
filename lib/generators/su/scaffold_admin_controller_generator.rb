2881b08a6891efe4eba319eea72850104fa25756
synthresin <synthresin@bagseong-wan-ui-MacBook-Pro.local>
2014-05-05 21:04:15 +0900

    폴더이름은 별로 영향을 안준다.

--- lib/generators/su/scaffold_admin_controller_generator.rb
+++ lib/generators/su/scaffold_admin_controller_generator.rb
@@ -3,6 +3,8 @@ require 'rails/generators/base'
 #module SunshineRailsGenerators
   class ScaffoldAdminControllerGenerator < Rails::Generators::Base
 
+  	namespace "su"
+
     def create_initializer_file
 	    create_file "config/initializers/initializer.rb", "# Add initialization content here"
 	  end
