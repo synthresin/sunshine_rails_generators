7bf9e3da50ad8af480c303096f48eeb84d22aa78
synthresin <synthresin@bagseong-wan-ui-MacBook-Pro.local>
2014-05-05 21:12:36 +0900

    폴더명, 모듈명은 알아볼 수 있게, 사용은 편하게.

--- lib/generators/sunshine_rails_generators/scaffold_admin_controller_generator.rb
+++ lib/generators/sunshine_rails_generators/scaffold_admin_controller_generator.rb
@@ -1,8 +1,10 @@
 require 'rails/generators/base'
 
-module Su
+module SunshineRailsGenerators
   class ScaffoldAdminControllerGenerator < Rails::Generators::Base
 
+  	namespace "su:scaffold_admin_controller"
+  	
     def create_initializer_file
 	    create_file "config/initializers/initializer.rb", "# Add initialization content here"
 	  end
