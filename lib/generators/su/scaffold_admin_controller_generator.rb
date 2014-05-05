9fe73935a4ddce5492a26dce6e5e291504a918ff
synthresin <synthresin@bagseong-wan-ui-MacBook-Pro.local>
2014-05-06 00:38:55 +0900

    테스팅

--- lib/generators/su/scaffold_admin_controller_generator.rb
+++ lib/generators/su/scaffold_admin_controller_generator.rb
@@ -4,12 +4,32 @@ module SunshineRailsGenerators
   class ScaffoldAdminControllerGenerator < Rails::Generators::NamedBase
 
   	namespace "su:scaffold_admin_controller"
-  	#source_root File.expand_path("../templates", __FILE__)
+  	source_root File.expand_path("../templates", __FILE__)
   	
-	  def copy_view_templates
-	    #copy_file "initializer.rb", "config/initializers/#{file_name}.rb"
-	    directory ".", "app/views/admin/#{plural_name}"
-	  end
+	  # def copy_view_templates
+	  #   #copy_file "initializer.rb", "config/initializers/#{file_name}.rb"
+	  #   directory ".", "app/views/admin/#{plural_name}"
+	  # end
+
+	  include ResourceHelpers
+
+    check_class_collision suffix: "Controller"
+
+    # class_option :orm, banner: "NAME", type: :string, required: true,
+    #                    desc: "ORM to generate the controller for"
+
+    # argument :attributes, type: :array, default: [], banner: "field:type field:type"
+
+    def create_controller_files
+      template "controller.rb", File.join('app/controllers', controller_class_path, "#{controller_file_name}_controller.rb")
+    end
+
+    # hook_for :template_engine, :test_framework, as: :scaffold
+
+    # Invoke the helper using the controller name (pluralized)
+    # hook_for :helper, as: :scaffold do |invoked|
+    #   invoke invoked, [ controller_name ]
+    # end
 
 
   end
