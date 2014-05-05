684a749aa6e278e5ec8f739bb7841a43041d42a1
synthresin <synthresin@bagseong-wan-ui-MacBook-Pro.local>
2014-05-05 21:06:12 +0900

    모듈 안으로 넣었다

--- lib/generators/su/scaffold_admin_controller_generator.rb
+++ lib/generators/su/scaffold_admin_controller_generator.rb
@@ -1,10 +1,10 @@
 require 'rails/generators/base'
 
-#module SunshineRailsGenerators
+module SunshineRailsGenerators
   class ScaffoldAdminControllerGenerator < Rails::Generators::Base
 
     def create_initializer_file
 	    create_file "config/initializers/initializer.rb", "# Add initialization content here"
 	  end
   end
-#end
+end
