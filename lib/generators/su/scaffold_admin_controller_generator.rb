c3de65f2f38884969c3ec1657fadacf296f4ebb9
synthresin <synthresin@bagseong-wan-ui-MacBook-Pro.local>
2014-05-05 21:02:50 +0900

    폴더 이름을 바꾸어보았어

--- lib/generators/su/scaffold_admin_controller_generator.rb
+++ lib/generators/su/scaffold_admin_controller_generator.rb
@@ -3,8 +3,6 @@ require 'rails/generators/base'
 #module SunshineRailsGenerators
   class ScaffoldAdminControllerGenerator < Rails::Generators::Base
 
-  	namespace "su"
-
     def create_initializer_file
 	    create_file "config/initializers/initializer.rb", "# Add initialization content here"
 	  end
