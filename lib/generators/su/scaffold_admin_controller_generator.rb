973b1cd5220a3dddc9b9264cf57f5b1c45d0a450
synthresin <synthresin@bagseong-wan-ui-MacBook-Pro.local>
2014-05-05 23:02:16 +0900

    템플릿이 되나 볼까?

--- lib/generators/su/scaffold_admin_controller_generator.rb
+++ lib/generators/su/scaffold_admin_controller_generator.rb
@@ -4,9 +4,10 @@ module SunshineRailsGenerators
   class ScaffoldAdminControllerGenerator < Rails::Generators::NamedBase
 
   	namespace "su:scaffold_admin_controller"
+  	source_root File.expand_path("../templates", __FILE__)
   	
-    def create_initializer_file
-	    create_file "config/initializers/initializer.rb", "# Add initialization content here"
+	  def copy_initializer_file
+	    copy_file "initializer.rb", "config/initializers/#{file_name}.rb"
 	  end
   end
 end
