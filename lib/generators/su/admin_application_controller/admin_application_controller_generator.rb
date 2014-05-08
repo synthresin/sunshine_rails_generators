cadbdf76b7a5822d96421cae702d60857e9cd911
synthresin <synthresin@bagseong-wan-ui-MacBook-Pro.local>
2014-05-08 17:28:12 +0900

    admin_install

--- lib/generators/su/admin_application_controller/admin_application_controller_generator.rb
+++ lib/generators/su/admin_application_controller/admin_application_controller_generator.rb
@@ -21,6 +21,10 @@ module SunshineRailsGenerators
 
     # argument :attributes, type: :array, default: [], banner: "field:type field:type"
 
+    def create_admin_controller_directory
+      empty_directory_with_keep_file 'app/controllers/admin'
+    end
+
     def create_controller_files
       template "controller.rb", File.join('app/controllers/admin', "application_controller.rb")
     end
---
+++ lib/generators/su/admin_install/admin_install_generator.rb
@@ -0,0 +1,15 @@
+require 'rails/generators/base'
+require 'rails/generators/resource_helpers'
+
+module SunshineRailsGenerators
+  class AdminInstallGenerator < Rails::Generators::Base
+
+  	namespace "su:admin_install"
+  	source_root File.expand_path("../templates", __FILE__)
+
+    Rails::Generators.invoke "su:admin_assets"
+    Rails::Generators.invoke "su:admin_layout"
+    Rails::Generators.invoke "su:admin_application_controller"
+
+  end
+end
