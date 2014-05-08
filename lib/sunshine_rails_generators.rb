d4adde74fb1c39af8243e29e8b652b614fad00b8
synthresin <synthresin@bagseong-wan-ui-MacBook-Pro.local>
2014-05-08 17:11:29 +0900

    아 모르게따

--- lib/sunshine_rails_generators.rb
+++ lib/sunshine_rails_generators.rb
@@ -2,5 +2,7 @@ require 'bootstrap-sass'
 require "sunshine_rails_generators/version"
 
 module SunshineRailsGenerators
-
+	def app_name
+		File.basename(destination_root)).tr('\\', '').tr(". ", "_")
+	end
 end
