dca2f396356a1f4faab3e1abc7224e27698fd113
synthresin <synthresin@bagseong-wan-ui-MacBook-Pro.local>
2014-05-08 18:42:09 +0900

    deleted comments

--- lib/generators/su/scaffold_admin_controller/scaffold_admin_controller_generator.rb
+++ lib/generators/su/scaffold_admin_controller/scaffold_admin_controller_generator.rb
@@ -7,11 +7,6 @@ module SunshineRailsGenerators
 
   	namespace "su:scaffold_admin_controller"
   	source_root File.expand_path("../templates", __FILE__)
-  	
-	  # def copy_view_templates
-	  #   #copy_file "initializer.rb", "config/initializers/#{file_name}.rb"
-	  #   directory ".", "app/views/admin/#{plural_name}"
-	  # end
 
 	  include Rails::Generators::ResourceHelpers
 
@@ -26,15 +21,8 @@ module SunshineRailsGenerators
       template "controller.rb", File.join('app/controllers/admin', "#{controller_file_name}_controller.rb")
     end
 
-    #hook_for :admin_application_controller#, as: :scaffold
-
-
     Rails::Generators.invoke "su:scaffold_admin_view"
 
-    # Invoke the helper using the controller name (pluralized)
-    # hook_for :helper, as: :scaffold do |invoked|
-    #   invoke invoked, [ controller_name ]
-    # end
 
 
   end
--- lib/generators/su/scaffold_admin_view/scaffold_admin_view_generator.rb
+++ lib/generators/su/scaffold_admin_view/scaffold_admin_view_generator.rb
@@ -7,11 +7,6 @@ module SunshineRailsGenerators
 
   	namespace "su:scaffold_admin_view"
   	source_root File.expand_path("../templates", __FILE__)
-  	
-	  # def copy_view_templates
-	  #   #copy_file "initializer.rb", "config/initializers/#{file_name}.rb"
-	  #   directory ".", "app/views/admin/#{plural_name}"
-	  # end
 
 	  include Rails::Generators::ResourceHelpers
 
@@ -34,14 +29,6 @@ module SunshineRailsGenerators
     def available_views
       %w(index edit show new _form)
     end
-
-    #hook_for :admin_application_controller#, as: :scaffold
-
-    # Invoke the helper using the controller name (pluralized)
-    # hook_for :helper, as: :scaffold do |invoked|
-    #   invoke invoked, [ controller_name ]
-    # end
-
 
   end
 end
