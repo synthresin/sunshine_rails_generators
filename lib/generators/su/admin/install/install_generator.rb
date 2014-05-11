41579675ba3d9965e3c491b58485c05f1c7d77f2
synthresin <synthresin@bagseong-wan-ui-MacBook-Pro.local>
2014-05-11 13:50:14 +0900

    Revert b0786bd..bdd3611
    This rolls back to commit b0786bda728e8283cc4cdd7f3e33266cd45c0754.

--- lib/generators/su/admin/install/install_generator.rb
+++ lib/generators/su/admin/install/install_generator.rb
@@ -2,18 +2,15 @@ require 'rails/generators/base'
 require 'rails/generators/resource_helpers'
 
 module SunshineRailsGenerators
-	module Admin
-		class InstallGenerator < Rails::Generators::Base
+  class InstallGenerator < Rails::Generators::Base
 
-	  	#namespace "su:admin:install"
-	  	source_root File.expand_path("../templates", __FILE__)
+  	namespace "su:admin:install"
+  	source_root File.expand_path("../templates", __FILE__)
 
-	    Rails::Generators.invoke "su:admin:assets"
-	    Rails::Generators.invoke "su:admin:layout"
-	    Rails::Generators.invoke "su:admin:partials"
-	    Rails::Generators.invoke "su:admin:application_controller"
+    Rails::Generators.invoke "su:admin:assets"
+    Rails::Generators.invoke "su:admin:layout"
+    Rails::Generators.invoke "su:admin:template"
+    Rails::Generators.invoke "su:admin:application_controller"
 
-	  end
-	end
-  
+  end
 end
