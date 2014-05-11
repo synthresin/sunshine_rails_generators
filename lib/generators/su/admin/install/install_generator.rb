bdd3611c18cfb6566ac9df0a7334ca2c9ee371c2
synthresin <synthresin@bagseong-wan-ui-MacBook-Pro.local>
2014-05-11 13:48:38 +0900

    오류

--- lib/generators/su/admin/install/install_generator.rb
+++ lib/generators/su/admin/install/install_generator.rb
@@ -2,15 +2,18 @@ require 'rails/generators/base'
 require 'rails/generators/resource_helpers'
 
 module SunshineRailsGenerators
-  class InstallGenerator < Rails::Generators::Base
+	module Admin
+		class InstallGenerator < Rails::Generators::Base
 
-  	#namespace "su:admin:install"
-  	source_root File.expand_path("../templates", __FILE__)
+	  	#namespace "su:admin:install"
+	  	source_root File.expand_path("../templates", __FILE__)
 
-    Rails::Generators.invoke "su:admin:assets"
-    Rails::Generators.invoke "su:admin:layout"
-    Rails::Generators.invoke "su:admin:partials"
-    Rails::Generators.invoke "su:admin:application_controller"
+	    Rails::Generators.invoke "su:admin:assets"
+	    Rails::Generators.invoke "su:admin:layout"
+	    Rails::Generators.invoke "su:admin:partials"
+	    Rails::Generators.invoke "su:admin:application_controller"
 
-  end
+	  end
+	end
+  
 end
